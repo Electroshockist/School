e71812ac9b61bb6bab65555374fd022a556258e6
Electroshockist <ericonfire2@yahoo.com>
2019-02-25 12:09:19 -0500

    Updated Mobile Programming Added fixed symbols added saving for quitting and rotating for symbols in first screen

--- year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/Game.java
+++ year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/Game.java
@@ -2,6 +2,8 @@ package electroshockist.lab6tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Game extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+
+    }
+
+    public void onSelect(View view) {
+
     }
 }
--- year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/MainActivity.java
+++ year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/MainActivity.java
@@ -1,21 +1,87 @@
 package electroshockist.lab6tictactoe;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String debugTag = "TicTacToeDebug";
+    private static final String debugTag = "TicTacToeDebug";
+
+    private static final String X = "X", O = "O", none = "none";
+
+    int[] xImages = {R.drawable.x, R.drawable.x_unselected};
+    int[] oImages = {R.drawable.o, R.drawable.o_unselected};
+
+    String selectedSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clearSelection();
     }
 
-    public void onSelect(View view){
-        Log.v(debugTag, "test");
+    public void SelectX(View view) {
+        selectedSymbol = X;
+        resolveImages();
+    }
+
+    public void SelectO(View view) {
+        selectedSymbol = O;
+        resolveImages();
+    }
+
+    void clearSelection(){
+        selectedSymbol = none;
+
+        ImageView ivX = findViewById(R.id.X);
+        ImageView ivO = findViewById(R.id.O);
+
+        ivX.setImageResource(R.drawable.x_unselected);
+        ivO.setImageResource(R.drawable.o_unselected);
+    }
+
+    void resolveImages(){
+        ImageView ivX = findViewById(R.id.X);
+        ImageView ivO = findViewById(R.id.O);
+
+        if(selectedSymbol.equals(X)){
+            ivX.setImageResource(R.drawable.x);
+            ivO.setImageResource(R.drawable.o_unselected);
+        }
+        else {
+            ivX.setImageResource(R.drawable.x_unselected);
+            ivO.setImageResource(R.drawable.o);
+        }
+
+    }
+
+    public void onPlay(View view) {
+        if (selectedSymbol.equals(none)){
+            Toast.makeText(this,"Please select a symbol",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //save
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.v(debugTag,"test");
+
+        outState.putString("selectedSymbol", selectedSymbol);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        selectedSymbol = savedInstanceState.getString("selectedSymbol");
+        resolveImages();
     }
 }
