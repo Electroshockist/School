355265ce076e9796de9e35ab0f3a50c573923357
Electroshockist <ericonfire2@yahoo.com>
2019-03-01 03:25:54 -0500

    Updated Mobile Programming Added Win Conditions
    Todo: figure out why win conditions returns to main menu

--- year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/Board.java
+++ year 2/semester 2/Mobile Programming/Lab6TicTacToe/app/src/main/java/electroshockist/lab6tictactoe/Board.java
@@ -1,12 +1,19 @@
 package electroshockist.lab6tictactoe;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.widget.Button;
 
+import java.util.Set;
+
 public class Board {
+    private static final String tie = "Tie";
+
     public boolean XTurn;
 
     Tile[][] grid;
+
+    String winningSymbol;
 
     public Board(Button[][] buttons) {
         XTurn = true;
@@ -27,8 +34,60 @@ public class Board {
     public boolean onClick(int y, int x, Context context){
 
         boolean returnValue = grid[y][x].onClick(XTurn, context);
-        XTurn = !XTurn;
+
+        if (returnValue) XTurn = !XTurn;
 
         return returnValue;
     }
+
+    public boolean checkEndConditions(){
+        Tile[] threeInARow = new Tile[3];
+
+        //check rows
+        for (int i = 0; i < grid.length; i++){
+            threeInARow = grid[i];
+        }
+        if (checkThreeInARow(threeInARow)) return true;
+
+        //check columns
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid.length; j++){
+                threeInARow[i] = grid[i][j];
+            }
+        }
+        if (checkThreeInARow(threeInARow)) return true;
+
+        //check forward diagonal
+        for (int i = 0; i < grid.length; i++){
+            threeInARow[i] = grid[i][i];
+        }
+
+        if (checkThreeInARow(threeInARow)) return true;
+
+        //check backward diagonal
+        for (int i = 0; i < grid.length; i++){
+            threeInARow[i] = grid[grid.length - i][grid.length - i];
+        }
+        if (checkThreeInARow(threeInARow)) return true;
+
+        return false;
+    }
+
+    //must be passed an array of size 3
+    private boolean checkThreeInARow(@NonNull Tile[] threeInARow){
+        if (threeInARow[0] == threeInARow[1] && threeInARow[0] == threeInARow[2]) return true;
+        return false;
+    }
+
+    private void setWinningSymbol(State state){
+        switch (state.symbol){
+            case Tile.XSymbol:
+                winningSymbol = "X";
+                break;
+            case Tile.OSymbol:
+                winningSymbol = "O";
+                break;
+        }
+    }
+
 }
