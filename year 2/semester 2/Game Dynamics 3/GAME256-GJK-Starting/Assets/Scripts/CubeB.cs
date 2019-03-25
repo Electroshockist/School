c63c54a9d50095603c03b31f0d5270aedb2a74e6
Electroshockist <ericonfire2@yahoo.com>
2019-03-25 14:47:27 -0400

    Added Game Dynamics 3 Added sinusoidal movement

---
+++ year 2/semester 2/Game Dynamics 3/GAME256-GJK-Starting/Assets/Scripts/CubeB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CustomRigidbody))]
+public class CubeB : MonoBehaviour {
+    CustomRigidbody rb;
+    float totaltime = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<CustomRigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        totaltime += Time.deltaTime;
+        rb.linear.V.x = -3 * Mathf.Sin(totaltime);
+        rb.linear.V.y = -3 * Mathf.Cos(totaltime);
+    }
+}
--- year 2/semester 2/Game Dynamics 3/GAME256-GJK-Starting/Assets/Scripts/CustomRigidbody.cs
+++ year 2/semester 2/Game Dynamics 3/GAME256-GJK-Starting/Assets/Scripts/CustomRigidbody.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomRigidbody : MonoBehaviour {
+    [Serializable]
+    public struct Linear {
+        public Vector3 A, V, D;
+    }
+    [Serializable]
+    public struct Angular {
+        public Vector3 A, V, D;
+    }
 
-    [SerializeField]
-    private Vector3 linA, linV, angA, angV;
-    private Vector3 linD, angD;
+    public Linear linear;
+    public Angular angular;
 
     private void Start() {
-        linD = transform.position;
-        angD = transform.eulerAngles;
+        linear.D = transform.position;
+        angular.D = transform.eulerAngles;
     }
 
     private void FixedUpdate() {
@@ -18,23 +26,23 @@ public class CustomRigidbody : MonoBehaviour {
     }
 
     Vector3 Move() {
-        linV += linA * Time.deltaTime;
-        linD += linV * Time.deltaTime;
+        linear.V += linear.A * Time.deltaTime;
+        linear.D += linear.V * Time.deltaTime;
 
-        return linD;
+        return linear.D;
     }
 
     Quaternion Rotate() {
-        angV += angA * Time.deltaTime;
-        angD += angV * Time.deltaTime;
+        angular.V += angular.A * Time.deltaTime;
+        angular.D += angular.V * Time.deltaTime;
 
         float cosYaw, sinYaw, cosPitch, sinPitch, cosRoll, sinRoll;
-        cosYaw = Mathf.Cos(angD.x / 2);
-        sinYaw = Mathf.Sin(angD.x / 2);
-        cosPitch = Mathf.Cos(angD.y / 2);
-        sinPitch = Mathf.Sin(angD.y / 2);
-        cosRoll = Mathf.Cos(angD.z / 2);
-        sinRoll = Mathf.Sin(angD.z / 2);
+        cosYaw = Mathf.Cos(angular.D.x / 2);
+        sinYaw = Mathf.Sin(angular.D.x / 2);
+        cosPitch = Mathf.Cos(angular.D.y / 2);
+        sinPitch = Mathf.Sin(angular.D.y / 2);
+        cosRoll = Mathf.Cos(angular.D.z / 2);
+        sinRoll = Mathf.Sin(angular.D.z / 2);
 
         float qX, qY, qZ, qW;
         qW = cosYaw * cosPitch * cosRoll + sinYaw * sinPitch * sinRoll;
