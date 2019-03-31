14116eecabc8c1d749b1edb84c8a29f653846508
Electroshockist <ericonfire2@yahoo.com>
2019-03-31 15:15:25 -0400

    Updated Data Structures Updated Lab 7

--- year 2/semester 2/Data structures/LAB7/LAB7/Controller.cpp
+++ year 2/semester 2/Data structures/LAB7/LAB7/Controller.cpp
@@ -2,18 +2,22 @@
 
 //Declare a static STL vector of Controller Pointers
 //Name it _instance
+vector<Controller*>Controller::_instance;
 
 
 Controller::Controller() 
 {
 	//Check if vector is empty
+	if (_instance.empty()) {
 
 
 	//Allocate vector memory by reserving the vector Controller locations
 	//Use vector's reserve(size) member function
-	
+		_instance.reserve(4);
+	}
 
 	cout << "Controller Constructor" << endl;
+
 }
 
 Controller::~Controller() 
@@ -24,20 +28,21 @@ Controller::~Controller()
 Controller* Controller::getInstance()
 {
 	//Check if the vector is less than 4
-	
+	if (_instance.size() < 4) {
 
-	//Instantiate a temporary auto_ptr for Controller
-		
+		//Instantiate a temporary auto_ptr for Controller
+		auto_ptr<Controller> tempController(new Controller());
 
-	//Push the controller pointer into the vector
-		
+		//Push the controller pointer into the vector
+		_instance.push_back(tempController.get());
 
-	cout << "Controller " << _instance.size() << " returned" << endl;
+		cout << "Controller " << _instance.size() << " returned" << endl;
+	}
 	
 	//Else 
-	
-	cout << "Controller Exists" << endl;
+	else cout << "Controller Exists" << endl;
 
 	//Return the last item in the vector
+	return _instance.back();
 	
 }
