2b8ac6e17e3d5db7a2695dae64efc1496f3ce5b6
Electroshockist <ericonfire2@yahoo.com>
2019-09-10 15:58:58 -0400

    Updated Game Development 2
    Added debug and timer

---
+++ year 3/semester 1/Game Development 2/Bi-Weekly Jam 1/Assets/Scripts/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public abstract class InputComponent {
+        public abstract void Gather(Data data);
+        public abstract void Input();
+    }
+    public abstract class ControlComponent {
+        public abstract void Gather(Data data);
+        public abstract void Execute();
+    }
+
+    [Header("Inputs")]
+    [SerializeField]
+    List<InputComponent> inputs = new List<InputComponent>();
+
+    [Header("Controls")]
+    [SerializeField]
+    List<ControlComponent> controls = new List<ControlComponent>();
+
+
+    private Data data = new Data();
+    // Start is called before the first frame update
+    void Start() {
+
+        InputComponent[] inputComponents = GetComponents<InputComponent>();
+        for (int i = inputComponents.Length - 1; i >= 0; i--) {
+            if (!inputs.Contains(inputComponents[i])) {
+                inputs.Add(inputComponents[i]);
+            }
+        }
+
+        ControlComponent[] controlComponents = GetComponents<ControlComponent>();
+        for (int i = controlComponents.Length - 1; i >= 0; i--) {
+            if (!controls.Contains(controlComponents[i])) {
+                controls.Add(controlComponents[i]);
+            }
+        }
+
+        foreach(InputComponent input in inputs) input.Gather(data);
+        foreach(ControlComponent control in controls) control.Gather(data);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (InputComponent input in inputs) input.Input();
+        foreach (ControlComponent control in controls) control.Execute();
+    }
+}
---
+++ year 3/semester 1/Game Development 2/Bi-Weekly Jam 1/Assets/Scripts/DataNode.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class DataNode {
+    public enum ValueType { NONE, INTERGER, FLOAT, VECTOR2 }
+    private object value;
+    private ValueType type;
+
+    public void Assign(int v) {
+        if (type == ValueType.NONE || type == ValueType.INTERGER || type == ValueType.FLOAT) {
+            type = ValueType.INTERGER;
+            value = v;
+        }
+    }
+
+    public void Assign(float v) {
+        if (type == ValueType.NONE || type == ValueType.INTERGER || type == ValueType.FLOAT) {
+            type = ValueType.FLOAT;
+            value = v;
+        }
+    }
+
+    public void Assign(Vector2 v) {
+        if (type == ValueType.NONE || type == ValueType.VECTOR2) {
+            type = ValueType.VECTOR2;
+            value = v;
+        }
+    }
+
+    public static explicit operator int(DataNode node) {
+        if (node.type == ValueType.INTERGER ||node.type == ValueType.FLOAT) {
+            return (int)node.value;
+        }
+        else return 0;
+    }
+
+    public static explicit operator float(DataNode node) {
+        if (node.type == ValueType.FLOAT || node.type == ValueType.FLOAT) {
+            return (float)node.value;
+        }
+        else return 0;
+    }
+
+    public static explicit operator Vector2(DataNode node) {
+        if (node.type == ValueType.NONE || node.type == ValueType.VECTOR2) {
+            return (Vector2)node.value;
+        }
+        else return Vector2.zero;
+    }
+}
