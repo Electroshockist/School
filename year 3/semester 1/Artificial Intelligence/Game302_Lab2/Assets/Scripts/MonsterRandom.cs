64ce0491d461faab7cc0d918c9ba97df122c423e
Electroshockist <ericonfire2@yahoo.com>
2019-09-23 19:09:26 -0400

    Updated Artificial Intelligence
    Finished lab 2
    Added lab 3

--- year 3/semester 1/Artificial Intelligence/Game302_Lab2/Assets/Scripts/MonsterRandom.cs
+++ year 3/semester 1/Artificial Intelligence/Game302_Lab2/Assets/Scripts/MonsterRandom.cs
@@ -23,87 +23,42 @@ public class MonsterRandom : GridMovement {
         // To do : Replace Random.Range(1,9) with Perlin Noise function
         // * FYI: Random.Range(int min, int max) returns a random interger number between min[inclusive] and max[exclusive]
         // * For more details about Random.Range, https://docs.unity3d.com/ScriptReference/Random.Range.html
-        int direction = Random.Range(1, 9);
+        int direction = (int)(1 + Mathf.PerlinNoise(transform.position.x, transform.position.y) * 7.9f);
 
-        float d = Mathf.PerlinNoise(GetPositionOnGrid().x, GetPositionOnGrid().y);
-        print(d + " " + GetPositionOnGrid());
-
-        if (d > 8 / 9) {
-            // Up Left
-            movement.x = -1;
-            movement.y = 1;
-
+        switch (direction) {
+            case 1: // Up Left
+                movement.x = -1;
+                movement.y = 1;
+                break;
+            case 2: // Up
+                movement.x = 0;
+                movement.y = 1;
+                break;
+            case 3: // Up Right
+                movement.x = 1;
+                movement.y = 1;
+                break;
+            case 4: // Right
+                movement.x = 1;
+                movement.y = 0;
+                break;
+            case 5: // Down Right
+                movement.x = 1;
+                movement.y = -1;
+                break;
+            case 6: // Down
+                movement.x = 0;
+                movement.y = -1;
+                break;
+            case 7: // Down Left
+                movement.x = -1;
+                movement.y = -1;
+                break;
+            case 8: // Left
+                movement.x = -1;
+                movement.y = 0;
+                break;
         }
-        else if (d > 7 / 9) {
-            // Up
-            movement.x = 0;
-            movement.y = 1;
-        }
-        else if (d > 6 / 9) {
-            // Up Right
-            movement.x = 1;
-            movement.y = 1;
-        }
-        else if (d > 5 / 9) {
-            // Right
-            movement.x = 1;
-            movement.y = 0;
-        }
-        else if (d > 4 / 9) {
-            // Down Right
-            movement.x = 1;
-            movement.y = -1;
-        }
-        else if (d > 3 / 9) {
-            // Down
-            movement.x = 0;
-            movement.y = -1;
-        }
-        else if (d > 2 / 9) {
-            // Down Left
-            movement.x = -1;
-            movement.y = -1;
-        }
-        else {
-            // Left
-            movement.x = -1;
-            movement.y = 0;
-        }
-
-        //switch (direction) {
-        //    case 1: // Up Left
-        //        movement.x = -1;
-        //        movement.y = 1;
-        //        break;
-        //    case 2: // Up
-        //        movement.x = 0;
-        //        movement.y = 1;
-        //        break;
-        //    case 3: // Up Right
-        //        movement.x = 1;
-        //        movement.y = 1;
-        //        break;
-        //    case 4: // Right
-        //        movement.x = 1;
-        //        movement.y = 0;
-        //        break;
-        //    case 5: // Down Right
-        //        movement.x = 1;
-        //        movement.y = -1;
-        //        break;
-        //    case 6: // Down
-        //        movement.x = 0;
-        //        movement.y = -1;
-        //        break;
-        //    case 7: // Down Left
-        //        movement.x = -1;
-        //        movement.y = -1;
-        //        break;
-        //    case 8: // Left
-        //        movement.x = -1;
-        //        movement.y = 0;
-        //        break;
-        //}
 
         positionOnGrid += movement;
         base.Update();
