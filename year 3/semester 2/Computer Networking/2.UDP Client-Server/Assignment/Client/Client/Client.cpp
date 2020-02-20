b34a15df3732ebeab54f23f4d3c08d12593b608a
Electroshockist <ericonfire2@yahoo.com>
2020-02-19 20:44:05 -0500

    Updated Computer networking
    Updated UDP assignment

--- year 3/semester 2/Computer Networking/2.UDP Client-Server/Assignment/Client/Client/Client.cpp
+++ year 3/semester 2/Computer Networking/2.UDP Client-Server/Assignment/Client/Client/Client.cpp
@@ -23,20 +23,69 @@ std::string cleanString(std::string string){
 	return (start == std::string::npos || end == std::string::npos) ? "" : string.substr(start, end);
 }
 
-std::vector<std::string> getInput(bool *doExit){
+std::vector<std::string> getInput(bool &doExit, bool needResponse){
 	std::vector<std::string> inputVector;
 	std::string input;
+
+	bool doSend = false;
 	do{
 		input.clear();
 		std::getline(std::cin, input);
-		if(!charsEqual(input.c_str(), SENDSYMBOL) || !input.empty()){
+		//check if "exit" was typed
+		doExit = charsEqual(input.c_str(), EXITSYMBOL);
+		//check if "send" was typed
+		doSend = charsEqual(input.c_str(), SENDSYMBOL);
+
+		//if we need to wait for a response, check if "exit" was recieved, then return
+		if(needResponse){
+			if(doSend){
+				printf("Cannot send: awaiting server response");
+			}
+
+			if(doExit){
+				inputVector.push_back(input);
+			}
+
+			return inputVector;
+		}
+		//if current line is not empty and is not equal to "send", add current line to input list
+		if(!doSend && !input.empty()){
 			inputVector.push_back(input.c_str());
 		}
+	}
+	//keep getting input unless "send" or "exit" was typed
+	while(!doSend && !doExit);
 
-	} while(charsEqual(input.c_str(), EXITSYMBOL) && charsEqual(input.c_str(), SENDSYMBOL));
-
-	*doExit = charsEqual(input.c_str(), EXITSYMBOL);
 	return inputVector;
+}
+
+bool sendStrings(SOCKET &sckt, sockaddr_in &server, std::vector<std::string> strings, int &size){
+	size = 0;
+	for(size_t i = 0; i < strings.size(); i++){
+		std::string s = std::to_string(i) + "|" + strings[i];
+		int sendOK = sendto(sckt, s.c_str(), s.size() + 1, 0, (sockaddr *)&server, sizeof(server));
+		if(sendOK == SOCKET_ERROR){
+			std::cout << "Failed to send: " << WSAGetLastError() << std::endl;
+			return false;
+		}
+		size += s.size();
+	}
+	return true;
+}
+
+bool getResponse(SOCKET sckt, int lastMessageSize){ 
+	sockaddr_in server;
+	int serverLength = sizeof(server);
+	char buf[1024];
+
+	ZeroMemory(&server, serverLength);
+	ZeroMemory(buf, 1024);
+
+	int bytesIn = recvfrom(sckt, buf, 1024, 0, (sockaddr *)&server, &serverLength);
+	if(bytesIn == SOCKET_ERROR){
+		std::cout << "Error receiving from the client " << WSAGetLastError() << std::endl;
+	}
+	return std::string(buf).size() == lastMessageSize;
 }
 
 int main(){
@@ -44,7 +93,7 @@ int main(){
 	WORD version = MAKEWORD(2, 2);
 	int wsOK = WSAStartup(version, &data);
 
-	bool exit = false;
+	bool exit = false, needResponse = false;
 
 	if(wsOK != 0){
 		std::cout << "Couldn't start winsock: " << wsOK << std::endl;
@@ -59,16 +108,16 @@ int main(){
 	server.sin_port = htons(54000);
 	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
 
-	std::vector<std::string> s;
 	do{
-		s = getInput(&exit);
-		if(!s.empty()){
-			int sendOK = sendto(sckt, s.c_str(), s.size() + 1, 0, (sockaddr *)&server, sizeof(server));
-			if(sendOK == SOCKET_ERROR){
-				std::cout << "Failed to send: " << WSAGetLastError() << std::endl;
-				return -1;
-			}
-		}
+		int lastMessageSize = -1;
+		if(!sendStrings(sckt, server, getInput(exit, needResponse), lastMessageSize)) return -1;
+
+		printf("Awaiting response...\n");
+
+		//save if response is correct
+		needResponse = !getResponse(sckt, lastMessageSize);
+		printf("Response recieved\n");
+
 	} while(!exit);
 
 	closesocket(sckt);
--- year 3/semester 2/Computer Networking/2.UDP Client-Server/Assignment/Server/Server/Server.cpp
+++ year 3/semester 2/Computer Networking/2.UDP Client-Server/Assignment/Server/Server/Server.cpp
@@ -18,13 +18,22 @@ std::string cleanString(std::string string){
 	size_t start = string.find_first_not_of(WHITESPACE);
 	size_t end = string.find_last_not_of(WHITESPACE);
 
-	return (start == std::string::npos || end == std::string::npos) ? "" : string.substr(start, end);
+	return (start == std::string::npos || end == std::string::npos) ? "" : string.substr(start, end + 1);
+}
+
+std::string parseString(std::string string){
+	size_t place = string.find_first_of("|", 0);
+	std::string s = string.substr(0, place);
+	std::cout << s << std::endl;
+	return s;
 }
 
 int main(){
 	WSAData data;
 	WORD version = MAKEWORD(2, 2);
 	int wsOK = WSAStartup(version, &data);
+
+	bool doExit = false;
 
 	if(wsOK != 0){
 		std::cout << "Couldn't start winsock: " << wsOK << std::endl;
@@ -38,6 +47,7 @@ int main(){
 	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
 	serverHint.sin_family = AF_INET;
 	serverHint.sin_port = htons(54000);
+	inet_pton(AF_INET, "127.0.0.1", &serverHint.sin_addr);
 
 	if(bind(sckt, (sockaddr *)&serverHint, sizeof(serverHint)) == SOCKET_ERROR){
 		std::cout << "Socket bind failed: " << WSAGetLastError() << std::endl;
@@ -48,7 +58,7 @@ int main(){
 	int clientLength = sizeof(client);
 
 	char buf[1024];
-	while(true){
+	do{
 		ZeroMemory(&client, clientLength);
 		ZeroMemory(buf, 1024);
 
@@ -57,20 +67,25 @@ int main(){
 			std::cout << "Error receiving from the client " << WSAGetLastError() << std::endl;
 			continue;
 		}
+		doExit = charsEqual(cleanString(std::string(buf)).c_str(), EXITSYMBOL);
+
 		char clientIP[256];
 
 		ZeroMemory(clientIP, 256);
 
 		inet_ntop(AF_INET, &client.sin_addr, clientIP, 256);
-
-		std::cout << EXITSYMBOL <<  "==" << cleanString(std::string(buf)).c_str() << ": " << (charsEqual(cleanString(std::string(buf)).c_str(), EXITSYMBOL) ? "true" : "false") << std::endl;
-		if(charsEqual(cleanString(std::string(buf)).c_str(), EXITSYMBOL)){
-			break;
+		if(!doExit){
+			// Display the message / who sent it
+			parseString(std::string(buf));
+			//std::cout << "Message recv from " << clientIP << " : " << buf << std::endl;
 		}
 
-		// Display the message / who sent it
-		std::cout << "Message recv from " << clientIP << " : " << buf << std::endl;
-	}
+		int bytesOut = sendto(sckt, buf, sizeof(buf), 0, (sockaddr *)&client, sizeof(client));
+		if(bytesOut == SOCKET_ERROR){
+			std::cout << "Error sending to the client " << WSAGetLastError() << std::endl;
+			continue;
+		}
+	} while(!doExit);
 
 	closesocket(sckt);
 	WSACleanup();
